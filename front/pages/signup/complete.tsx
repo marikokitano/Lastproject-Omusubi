fbd56b5a08f5c8d9733e780acb57dbd19282386b
haruka-narikawa <narikawa.h@outlook.jp>
2023-06-01 16:57:18 +0900

    コミット3回目
    ファミリーのディレクトリを作成

--- front/pages/signup/complete.tsx
+++ front/pages/signup/complete.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import Logo from '@/components/Logo';
 import Button from '@/components/Button';
 
 const Complete = () => {
-    const handleClick = () => {
-        console.log('ボタンがクリックされました！');
+    const router = useRouter();
+    const ClickTop = () => {
+        console.log('TopページのURL');
         };
+    const ClickFamily = () => {
+            router.push('http://localhost:3000/family/create');
+            };
     return(
         <div className='text-center items-center'>
             <Logo />
             <p className='text-lg font-medium pb-5 pt-10'>プロフィールの登録完了</p>
             <p className='pb-10'>プロフィールの登録が完了しました！</p>
             <div className='flex justify-center'>
-                <Button onClick={handleClick} text="TOPページへ" />
+                <Button onClick={ClickTop} text="TOPページへ" />
             </div>
             <p className='pt-20 pb-10'>引き続き、家族を登録しましょう</p>
             <div className='flex justify-center'>
-                <Button onClick={handleClick} text="家族を登録" />
+                <Button onClick={ClickFamily} text="家族を登録" />
             </div>
         </div>
     );
