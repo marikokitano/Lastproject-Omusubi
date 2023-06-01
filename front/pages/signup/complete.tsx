873128b834583a8c78021138b73bbfe0a700243f
haruka-narikawa <narikawa.h@outlook.jp>
2023-06-01 13:41:35 +0900

    コミット3回目
    ページ遷移、プルダウン作成

--- front/pages/signup/complete.tsx
+++ front/pages/signup/complete.tsx
@@ -11,9 +11,13 @@ const Complete = () => {
             <Logo />
             <p className='text-lg font-medium pb-5 pt-10'>プロフィールの登録完了</p>
             <p className='pb-10'>プロフィールの登録が完了しました！</p>
-            <Button onClick={handleClick} text="TOPページへ" />
+            <div className='flex justify-center'>
+                <Button onClick={handleClick} text="TOPページへ" />
+            </div>
             <p className='pt-20 pb-10'>引き続き、家族を登録しましょう</p>
-            <Button onClick={handleClick} text="家族を登録" />
+            <div className='flex justify-center'>
+                <Button onClick={handleClick} text="家族を登録" />
+            </div>
         </div>
     );
 };
--- front/pages/signup/index.tsx
+++ front/pages/signup/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import Logo from '@/components/Logo';
+import Dropdown from '@/components/Dropdown';
+
 
 const SignUp = () => {
-const handleClick = () => {
- console.log('ボタンがクリックされました！');
+    const router = useRouter();
+    const handleClick = () => {
+        router.push("http://localhost:3000/signup/complete");
  };
 
  return (
  <div className='text-center items-center'>
     <Logo />
+    <div className=' font-medium'>
     <p className='pt-10'>認証に成功しました</p>
     <p className='pb-10'>以下からプロフィール登録をして下さい</p>
     <p className='pb-10'>プロフィール登録</p>
@@ -20,8 +25,8 @@ const handleClick = () => {
     label="フリガナ"/>
     <Input 
     label="郵便番号"/>
-    <Input 
-    label="都道府県"/>
+    <p>都道府県</p>
+    <Dropdown />
     <Input 
     label="市区町村"/>
     <Input 
@@ -30,7 +35,10 @@ const handleClick = () => {
     label="アパート・マンション名"/>
     <Input 
     label="電話番号"/>
-    <Button onClick={handleClick} text="登録" />
+    </div>
+    <div className='pt-10 pb-10 flex justify-center'>
+    <Button onClick={handleClick} text="登録する" />
+    </div>
     </div>
     );
 };
