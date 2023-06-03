2b61413fb76e715da14471e412d192f4d46863e3
Goto Reiko <7065391+gotoreiko@users.noreply.github.com>
2023-06-03 10:39:58 +0900

    subscriptionsテーブルの実装（get、create）

--- api/main.go
+++ api/main.go
@@ -51,24 +51,18 @@ func main() {
 		})
 	}
 
-  
 	r.HandleFunc("/users", handlers.GetUsers(db)).Methods("GET")
 	r.HandleFunc("/users/{id}", handlers.GetUser(db)).Methods("GET")
 	r.HandleFunc("/users", handlers.CreateUsers(db)).Methods("POST")
 	r.HandleFunc("/buy", stripeHandler.CreateCheckoutSession(db)).Methods("POST")
-	// r.HandleFunc("/sidedishes", handlers.GetSidedishes(db)).Methods("GET")
-	// r.HandleFunc("/sidedishes", handlers.CreateSidedish(db)).Methods("POST")
-	// r.HandleFunc("/sidedish/{id}", handlers.GetSidedish(db)).Methods("GET")
-	// r.HandleFunc("/sidedish/{id}", handlers.PatchSidedish(db)).Methods("PATCH")
-	// r.HandleFunc("/sidedish/{id}", handlers.DeleteSidedish(db)).Methods("DELETE")
 	r.HandleFunc("/plans", handlers.GetPlans(db)).Methods("GET")
 	r.HandleFunc("/plans", handlers.CreatePlan(db)).Methods("POST")
 	r.HandleFunc("/plan/{id}", handlers.GetPlan(db)).Methods("GET")
 	r.HandleFunc("/plan/{id}", handlers.PatchPlan(db)).Methods("PATCH")
 	r.HandleFunc("/plan/{id}", handlers.DeletePlan(db)).Methods("DELETE")
-	r.HandleFunc("/orderdetails", handlers.GetOrderdetails(db)).Methods("GET")
-	r.HandleFunc("/orderdetail/{id}", handlers.GetOrderdetail(db)).Methods("GET")
-	r.HandleFunc("/orderdetails", handlers.CreateOrderdetail(db)).Methods("POST")
+	r.HandleFunc("/subscriptions", handlers.GetSubscriptions(db)).Methods("GET")
+	r.HandleFunc("/subscription/{id}", handlers.GetSubscription(db)).Methods("GET")
+	r.HandleFunc("/subscription", handlers.CreateSubscription(db)).Methods("POST")
 	http.ListenAndServe(":8080", corsMiddleware(r))
 
 }
