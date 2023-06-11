acc161712d452ba30e9c8893e9ada25ed71aeedc
haruka-narikawa <narikawa.h@outlook.jp>
2023-06-11 17:06:00 +0900

    ボタンの色、Topページ変更
    ボタンの色とTopページを一部変更しました。

--- front/components/Plan.tsx
+++ front/components/Plan.tsx
@@ -28,24 +28,29 @@ type Props = {
 };
 
 // 商品一覧ページ
+
 const Plan: NextPage<Props> = ({ planList, family }) => {
 	return (
 		<>
-			<h2>PLAN</h2>
-			<h3>おかず</h3>
-			<div>
-				{planList ? (
-					<ul>
-						{planList.map((plan) => (
-							<li key={plan.id}>
-								<PlanItem plan={plan} family={family} />
-							</li>
-						))}
-					</ul>
-				) : (
-					<p>準備中...</p>
-				)}
+		<div className="container mt-10 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+			<div className="flex items-center space-x-2 mb-5">
+				<h2 className="second-title">PLAN</h2>
+				<h3 className="text-xs bg-red text-white px-2 py-1">おかず</h3>
 			</div>
+				<div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3  mb-10">
+					{planList ? (
+						<ul>
+							{planList.map((plan) => (
+								<li key={plan.id}>
+									<PlanItem plan={plan} family={family} />
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>準備中...</p>
+					)}
+				</div>
+			</div>	
 		</>
 	);
 };
