435acdccae6ed6049b00010557a86670d37ba0f1
Goto Reiko <7065391+gotoreiko@users.noreply.github.com>
2023-06-11 11:25:31 +0900

    不要なファイルと記述を削除

--- front/pages/invite/index.tsx
+++ front/pages/invite/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Button from '@/components/Button';
 import Layout from '@/components/Layout';
-import Input from '@/components/Input';
 
 
 const Invite = () => {
@@ -11,22 +10,15 @@ const Invite = () => {
         console.log("編集する")
     }
     return (
-        <div className='text-center items-center'>
             <Layout>
+        <div className='text-center items-center'>
                 
-            </Layout>
             <div className='font-medium'>
                 <p className='text-lg pt-10 pb-20'>家族を招待</p>
-                <Input 
-                label="招待する家族のemail"/>
-                <div className='flex justify-center pt-10'>
-                    <Button 
-                        onClick={handleClick} 
-                        text="家族を招待する"></Button>
-                </div>
             </div>
             
         </div>
+            </Layout>
     )
 }
 
