6428c0c826138e05f61208ec65cbc44227cfd101
Ai1029 <ai.ishiwata1029.s@gmail.com>
2023-06-12 14:57:39 +0900

    コンフリクトを解消

--- front/components/Delivery.tsx
+++ front/components/Delivery.tsx
@@ -10,7 +10,6 @@ const Delivery = () => {
   console.log(familyData);
   return (
     <>
-<<<<<<< HEAD
       <div className="container mt-10 items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
         <h1 className="second-title-ja mb-5">次回のお届けプラン</h1>
         <section className="w-fll">
@@ -48,68 +47,6 @@ const Delivery = () => {
                         <p className="text-sm text-gray-500 leading-6 mb-5">
                           {item.plan.explanation}
                         </p>
-=======
-      <div className="container mt-10 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
-        {myData ? (
-          <div>
-            <p>自宅にお届け</p>
-            {myData.map((item, index) => (
-              <div key={index}>
-                <p>{item.plan.name}</p>
-                <div>
-                  <img className="object-cover w-full h-48 rounded-lg mt-4" src={item.plan.image} alt="" />
-                </div>
-                <p>{item.plan.explanation}</p>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            <p>自宅にお届けする商品はまだありません</p>
-          </div>
-        )}
-        {familyData ? (
-          <div>
-            <p>家族にお届け</p>
-            {familyData.map((item, index) => (
-              <div key={index}>
-                <p>{item.received_user.name}さんにお届け</p>
-                <p>{item.plan.name}</p>
-                <div>
-                  <img className="object-cover w-full h-48 rounded-lg mt-4" src={item.plan.image} alt="" />
-                </div>
-                <p>{item.plan.explanation}</p>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            <p>家族にお届けする商品はまだありません</p>
-          </div>
-        )}
-        {/*
-          <div key={index}>
-            <section>
-              <div className="lg:grid max-w-70 mx-auto mt-12 md:grid-cols-2 lg:grid-cols-3 lg:gap-10">
-                <div className="col-span-2 lg:col-span-2 flex flex-col mb-3 overflow-hidden lg:2/3">
-                  <div className="flex-shrink-0">
-                    <h1 className="text-base font-bold text-white bg-font-yellow py-1 px-4 mb-5">次回のお届けプラン</h1>
-                    <p>2023年6月16日（金）配送予定</p>
-                    <p>{item.received_user.name}様にお届け</p>
-                    <img className="object-cover w-full h-55 rounded-lg mt-4" src={item.plan.image} alt="" />
-                  </div>
-
-                  <div className="flex flex-col justify-between flex-1">
-                    <div className="flex-1">
-                      <div className="flex pt-2 space-x-1 text-xs text-gray-500">
-                        <span> ※写真はイメージです </span>
-                      </div>
-
-                      <div className="block mt-4">
-                        <h3 className="text-2xl font-semibold leading-none tracking-tighter text-gray-600 mb-4">{item.plan.name}</h3>
-                        <p className="text text-gray-500 leading-6">{item.plan.nextdelivery}</p>
-                        <p className="text text-gray-500 leading-6">数量：1</p>
->>>>>>> main
                       </div>
                     ))}
                   </div>
@@ -165,7 +102,6 @@ const Delivery = () => {
               </div>
             </div>
           </div>
-<<<<<<< HEAD
         </section>
         {myData ? (
           <div className="flex justify-center items-center mb-20">
@@ -176,15 +112,7 @@ const Delivery = () => {
         ) : (
           <div></div>
         )}
-=======
-                     */}
       </div>
-      {/*
-      <div className="flex justify-center items-center mb-20">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-md">プランを変更する</button>
->>>>>>> main
-      </div>
-      */}
     </>
   );
 };
--- front/components/Orderhistory.tsx
+++ front/components/Orderhistory.tsx
@@ -11,7 +11,6 @@ const Orderhistory = () => {
         <section>
           <p className="second-title-ja mb-5">注文履歴</p>
           {data ? (
-<<<<<<< HEAD
             <div className="flex flex-col">
               <div className="-m-1.5 overflow-x-auto">
                 <div className="p-1.5 min-w-full inline-block align-middle">
@@ -86,63 +85,6 @@ const Orderhistory = () => {
               <p>注文履歴はありません</p>
             </div>
           )}
-=======
-            <div>
-              {data.map((item, index) => (
-                <div key={index}>
-                  <p>{item.payment_date}これは注文日なので計算して配送日を入れるかそのままにするか</p>
-                  <p>{item.plan_name}</p>
-                  <p>{item.received_user.name}さんにお届け</p>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div>
-              <p>注文履歴はありません</p>
-            </div>
-          )}
-
-{/*
-          <div className="flex flex-col">
-            <div className="-m-1.5 overflow-x-auto">
-              <div className="p-1.5 min-w-full inline-block align-middle">
-                <div className="border rounded-lg overflow-hidden dark:border-gray-700">
-                  <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                    <thead className="bg-gray-50 dark:bg-gray-700">
-                      <tr>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">
-                          配送日
-                        </th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">
-                          注文プラン
-                        </th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">
-                          宛先
-                        </th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">
-                          おすすめ度
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                      <tr>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">2023/5/25</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">ワクワクプラン</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">〇〇様に〇〇個、〇〇様に〇〇個お届け</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                          <a className="text-blue-500 hover:text-blue-700" href="#">
-                            おすすめ度の評価
-                          </a>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-*/}
->>>>>>> main
         </section>
       </div>
     </>
